39a0ffbd75f4ff95dc1efe8c70afd94f864fae02
Yana <yanaegorova69.3@gmail.com>
2021-10-25 19:15:36 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,305 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Titanium Spinning Top',
+        price: 19.99,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Timeless Art',
+                text: 'Your spinning top will forever remain a timeless, elegant piece of art that will be loved by you and your children\'s children.',
+            },
+            {
+                title: 'Precision-Machined',
+                text: 'Every top is checked for perfection. Tops are precision-machined using only the purest metals and alloys from around the world.',
+            },
+            {
+                title: 'Boost Creativity & Focus',
+                text: 'According to recent studies, fiddling with items at your desk can aid in thought process and improve productivity!',
+            },
+            {
+                title: 'Challenge Yourself',
+                text: 'We all love to challenge ourselves. Beat your own spin times or, better yet, compete with your friends for pizza or drinks.',
+            },
+            {
+                title: 'The Perfect Size',
+                text: 'Dimensions expertly calculated to allow for minimal friction and stress on the tops\' tips while still allowing handling with hands of all sizes. 1.4 tall" x 1.125" diameter. 0.6oz.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.jpg'],
+    },
+    {
+        id: 'product-2',
+        name: 'Transforming Sword',
+        price: 24.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Dynamic tool transforms from pickaxe to sword with one swift motion.',
+            },
+            {
+                text: 'Mine for ore then fight mobs then go back to mining!',
+            },
+            {
+                text: 'Exciting two-in-one tool has brilliant new diamond design!',
+            },
+            {
+                text: 'Pickaxe/Sword is large and feels great in your hands!',
+            },
+            {
+                text: 'Be prepared for anything with this great two-in-one, multi-function item inspired by the World of Minecraft.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Educational Counting Toy',
+        price: 35.32,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'TEACH children to count to 100 by ones using these colorful cubes.',
+            },
+            {
+                text: 'EXPLORE shapes and area by linking the cubes together on all sides.',
+            },
+            {
+                text: '10 different kid-friendly colors, these cubes are terrific for grouping by color and for exploring patterns.',
+            },
+            {
+                text: 'Set offers 500 pieces total (50 pieces in each of the 10 colors.) so up to 5 children can practice counting.',
+            },
+            {
+                text: 'For ages 5+',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Costume',
+        price: 39.99,
+        image: './img/img4.png',
+        description: [
+            {
+                text: '100% Polyester.',
+            },
+            {
+                text: 'Product Includes: Jumpsuit with attached boot covers, detachable tail, pair of gloves & soft headpiece.',
+            },
+            {
+                text: 'Deluxe costume including attached boot covers and pair of gloves.',
+            },
+            {
+                text: 'Officially Licensed product.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.jpg'],
+    },
+    {
+        id: 'product-5',
+        name: '4 Connect in a Row',
+        price: 43.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Durable giant wooden board 31" x 23" made with premium wood and finished with natural stain. Game stands 2.5 feet tall.',
+            },
+            {
+                text: 'Complete wooden construction with beautifully finished legs, frame and bottom bar make for a perfect addition to any gathering which will entertain friends and family for hours.',
+            },
+            {
+                text: 'Includes durable nylon carrying case which holds the 42 solid plastic coins, 21 red and 21 blue measuring 3.5" diameter for perfect game play and sliding action.',
+            },
+            {
+                text: 'Giant game is easy to set up indoors our outside and easily transportable and zips up with easy handles and pockets to fit all of your game pieces.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.jpg', './img/img5-4.jpg'],
+    },
+    {
+        id: 'product-6',
+        name: 'Brybelly Go',
+        price: 49.95,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Classic Strategy Game',
+                text: 'Originating 4,000 years ago, Go is one of the most complex games in the world.',
+            },
+            {
+                title: 'Bamboo Go Board',
+                text: 'Traditional 47x44cm oblong table board, 2cm thick, made with pressed, natural bamboo.',
+            },
+            {
+                title: 'Reversible Board',
+                text: 'Standard 19x19 playing field on the top, 13x13 beginner\'s board on the bottom.',
+            },
+            {
+                title: '361 Bakelite Stones',
+                text: 'Two bamboo bowls hold the included 361 single-convex bakelite stones (180 white, 181 black) aka goishi stones.',
+            },
+            {
+                title: 'Premium Quality',
+                text: 'Matching, dark wash wood stain; deep, engraved lines; total product weight over 11 pounds.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Pool For Kids',
+        price: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'Features two molded-in seats in the shade and the designs molded-in to the bottom of the pool provide traction.',
+            },
+            {
+                text: 'Relax in the shade of the included 40 in. (1.02 m.) umbrella that nests on the side of the pool!',
+            },
+            {
+                text: 'Includes 3 funnel cups and a fun spinning water wheel that attach easily to the umbrella post for added play.',
+            },
+            {
+                text: 'Holds up to 12 gal. (45.4 L) of water or 165 lbs. (74.8 kg.) of sand (water and sand not included).',
+            },
+            {
+                text: 'Maximum weight per seat: 50 lbs. (22.67 kg.)',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png','./img/img7-4.jpg'],
+    },
+    {
+        id: 'product-8',
+        name: 'Ball Pool',
+        price: 79.99,
+        image: './img/img8.png',
+        description: [
+            {
+                title: 'Selected High-Quality Material',
+                text: 'Foam ball pit are made of environmentally friendly and healthy high-quality memory foam, soft and durable (not contain any harmful substances), not easy deform, ideal for babies and children. Smooth surface and non-slip bottom provides a safe and comfortable place your little one play. At the same time, the classic light gray design fits perfectly with most home environments.',
+            },
+            {
+                title: 'Fodable & Portable Design',
+                text: 'The bottom of the ball pits for toddlers designed with a secure zipper, so it can be easily folded and installed, and when you are not using it, just fold it into the cabinet for storage. Thanks to the lightweight material, you can easily take it out and use it in places like the grass or park, or use it as a prop for your baby\'s photo shoot.',
+            },
+            {
+                title: 'Children’s Private Space',
+                text: 'The ball pit for baby would be one of the best toys for children in the home, where children can release their emotions and play happily, becoming the child\'s private space. It will also free up mom\'s hands and give her more time to finish own things. Because of its large size (the thickness is about 1.96”, and 35.4” in inner diameter, 11.8” in height), it is also more than just a ball pit, it can also accommodate more other toys, It\'s a great storage venue.',
+            },
+            {
+                title: 'Wonderful Gift For Kids & Mom',
+                text: 'The ball pit is a top choice for a child\'s birthday gift. The popular and stylish design of foam ball pit makes it a perfect photo prop or decorative item for the living room and bedroom as well. Perfect for giving to your loved ones and friends on holidays such as baby showers or bridal showers.',
+            },
+            {
+                title: 'Easy Clean & Customer First',
+                text: 'The ball pits for toddlers cover is machine washable with zip design, easier to remove and clean. We focus on product quality and value the shopping experience of our customers highly, the foam ball pit has passed strict quality inspection. If you have any question or suggestion about the items you have received, please contact us, we promise we will give you a satisfy reply.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.jpg'],
+    },
+    {
+        id: 'product-9',
+        name: 'Tree Swing',
+        price: 84.41,
+        image: './img/img9.png',
+        description: [
+            {
+                title: '40” Saucer Mat Swing ((Black/Multi-Color)',
+                text: 'Provides a fun hang out spot for kids (and adults) to safely swing into action for playtime or relaxation — Fits 2-3 kids.',
+            },
+            {
+                title: 'For Indoor/Outdoor Fun',
+                text: 'Great for backyards, playgrounds, indoor play areas, etc — Low grade weatherproof — Can be used as chair swing for tree, hammock swing for swingset, nest swing chair, swings for playsets — Fun way to play, lounge, and enjoy imaginative play with friends and family — Gentle swing movement encourages balance, supports sensory needs, hyperactivity, and more — Makes a great gift — Perfect for parties and play dates.',
+            },
+            {
+                title: 'Comfy Hanging Seat',
+                text: 'Mat seat provides super comfy and sturdy ride — Holds up to 700 lbs for safe riding experience — Provides superior strength while keeping swing in place.',
+            },
+            {
+                title: 'Quick & Easy Assembly',
+                text: 'Hangs from 2 points for swinging — Top of ropes have steel rings which connect to adjustable suspension rope up to 63” long from seat to connection point — Hangs almost anywhere whether mounted from tree, swing set, or beam in the house — Designed for secure installation.',
+            },
+            {
+                title: 'Sturdy Design & Padded Frame',
+                text: 'Seat made of heavy-duty (PP) polypropylene and oxford fabric in black — Multi-color rainbow frame, orange, yellow, green, blue, purple — Rope made of PE (Polyethylene) — Steel tube frame padded with NBR foam (approx. 1.6” thick) — Includes saucer swing, flags, hardware, and instructions — Adult supervision and assembly required — Recommended for children 3+ years — Maximum weight is 330 pounds — Spot clean — Maximum Tilt is 45° — Measures approx 40” D x 63” H.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png', './img/img9-4.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Baby Walker',
+        price: 89.99,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Perfect walker and 5-in-1 play center for toddlers ages 12+ months. Premium natural design with tastful pastel colors.',
+            },
+            {
+                text: 'Suitable for children 12 months +',
+            },
+            {
+                text: 'High quality wood with rounded edges and painted with kids safe non-toxic water based paint.',
+            },
+            {
+                text: 'Comes in an attractive colorful packaging for gifting.',
+            },
+            {
+                text: 'Exclusive Small Foot wooden toy design.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Mini Trampoline',
+        price: 119.98,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'Best Birthday Gifts For Toddler/Kid/Child',
+                text: 'This children/toddler trampoline is sturdy and very bouncy with an dinosaur feature and woodland camo design, perfect jumpsport gift for the young dinosaur fan or an active kid for a trampolining exercise. It will be a ton of fun for your kids on recreational trampolines, jumping for joy!',
+            },
+            {
+                title: 'Good For Kids Growth',
+                text: 'A great mini trampoline for kids to have after-school fun and do exercise in a way that develops their strength, balance, stamina and coordination while burning off pent-up energy. Construction can support weight up to 220lbs, all our products are tested to meet US safety standards.',
+            },
+            {
+                title: 'Easy Assemble & Space Saver',
+                text: 'Foldable design, assemble or disassemble in a minute, no tools required. This trampoline weighs under 18 lbs with folding function, convenient to carry and transport from place to place and easily store it in order to save space in all seasons.',
+            },
+            {
+                title: 'Quality Design & Safe',
+                text: 'This trampoline has a great bounce, made of a sturdy steel frame, a durable jumping mat and 30 of tension quiet bands, ultra-quiet, forgiving shock absorption, smooth rebound, no squeaks bounce. Rubber-tipped legs prevent scuffing of non-carpeted floors. In addition, the foam-covered handle and the protective cover provide all-round protections for your children\' safe and secure jumping.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png', './img/img11-4.png', './img/img11-5.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Basketball System',
+        price: 148.00,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Complete mini-basketball system includes height-adjustable backboard or rim, pole and mobile base.',
+            },
+            {
+                text: 'Has the look, functionality & durability of a professional-grade basketball system.',
+            },
+            {
+                text: 'Height adjusts up to 7\' for court- side play and down to 3\' for junior or poolside play.',
+            },
+            {
+                text: '33" x 23" clear polycarbonate shatterproof backboard with spring action break-away 14.5\'\' rim.',
+            },
+            {
+                text: 'Weighted mobile base (31.6 x 20 x 6.3") fills with sand and features built-in wheels for easy portability.',
+            },
+        ],
+        images: ['./img/img12-1.png','./img/img12-2.png','./img/img12-3.png'],
+    },
+]
